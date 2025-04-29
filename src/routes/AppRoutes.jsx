3c024109378910d110b546a48ea8b30a7f83643d
Petra Bayu Pangestu <petrabayu19@gmail.com>
2025-04-29 10:58:26 +0700

    Chore: Delete component and unused code

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,7 +5,6 @@ import DashboardLayout from "../components/DashboardLayout";
 import HomePage from "../pages/HomePage";
 import CameraListPage from "../pages/CameraListPage";
 import PairwiseComparisonPage from "../pages/PairwiseComparisonPage";
-import HistoryPage from "../pages/HistoryPage";
 import ChooseCameraPage from "../pages/ChooseCameraPage";
 import RankingPage from "../pages/RankingPage";
 import AboutPage from "../pages/AboutPage";
@@ -19,7 +18,6 @@ const AppRoutes = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/camera-list" element={<CameraListPage />} />
           <Route path="/comparison" element={<PairwiseComparisonPage />} />
-          <Route path="/history" element={<HistoryPage />} />
           <Route path="/choose-camera" element={<ChooseCameraPage />} />
           <Route path="/ranking" element={<RankingPage />} />
           <Route path="/about" element={<AboutPage />} />
