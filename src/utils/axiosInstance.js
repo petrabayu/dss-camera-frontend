4f9750a861e1d9ea52535cc58667b71cc6110828
Petra Bayu Pangestu <petrabayu19@gmail.com>
2025-05-01 14:32:24 +0700

    Fix: change localhost endpoint

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api", // Sesuaikan URL dengan backend Anda
+  baseURL: "http://localhost:3000/api", // Sesuaikan URL dengan backend Anda
   headers: {
     "Content-Type": "application/json",
   },
